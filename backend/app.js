e94fbaea7559b9d18cdedaeda777d06be87fb815
yashrepogit <yas@cloudify.biz>
2026-01-09 23:28:59 +0530

    added home route

--- backend/app.js
+++ backend/app.js
@@ -8,6 +8,12 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.get("/", (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "Hello Sardinaz App 🚀 Backend is running successfully!",
+  });
+});
 app.use("/v1/api/users", require("./routes/userAuth.routes"));
 app.use("/v1/api/admin", require("./routes/adminAuth.routes"));
 app.use("/v1/api/bookings", require("./routes/booking.routes"));
